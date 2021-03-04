257952c4c7ef269a6a3935af18e20c1c131666c7
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-03-04 21:51:20 +0300

    Update trustlessLGE.sol

--- contracts/trustlessLGE.sol
+++ contracts/trustlessLGE.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.6.0;
 
 // Author: Sam Porter
 
+// Or you could call it trust minimized lge.
 // What CORE team did is something really interesting, with LGE it's now possible 
 // to create fairer distribution and fund promising projects without VC vultures at all.
 // Non-upgradeable, not owned, liquidity is being created automatically on first transaction after last block of LGE.
@@ -9,7 +10,6 @@ pragma solidity >=0.6.0;
 // The Event lasts for ~2 months to ensure fair distribution.
 // 0,5% of contributed Eth goes to developer for earliest development expenses including audits and bug bounties.
 // Blockchain needs no VCs, no authorities.
-// Minimized the code a bit in case if publishing a contract will cost a small fortune for you, will publish trustlessLGEmin.sol too
 
 import "./SafeMath.sol";
 import "./IUniswapV2Factory.sol";
