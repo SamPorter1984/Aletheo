501ad05991aba767d55b23b1e2f49fd301e8af4b
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-03-19 00:15:04 +0300

    Update and rename NamelessProxy.sol to TrustMinimizedProxy.sol

--- contracts/TrustMinimizedProxy.sol
+++ contracts/TrustMinimizedProxy.sol
@@ -1,60 +1,68 @@
 pragma solidity >=0.7.0 <0.9.0;
 
+// EIP-1984: this is big for DeFi, but certainly not as big for eips in general, either way I hope it becomes a standard which will allow to easily distinguish
+// genuine anonymous devs from scammers.
+
 // OpenZeppelin Upgradeability contracts modified by Sam Porter. Proxy for Nameless Protocol contracts
 // You can find original set of contracts here: https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/proxy
 
-// Had to pack OpenZeppelin upgradeability contracts in one single contract for readability. Some differences:
+// Had to pack OpenZeppelin upgradeability contracts in one single contract for readability. It's basically the same OpenZeppelin functions 
+// but in one contract with some differences:
 // 1. DEADLINE is a block after which it becomes impossible to upgrade the contract. Defined in constructor and here it's ~2 years.
-// Maybe not even required for most contracts, but I kept it as an option.
-// 2. UPGRADE_BLOCK defines how often the contract can be upgraded. Defined in _setNextLogic() function and the interval here is set
+// Maybe not even required for most contracts, but I kept it in case if something happens to developers.
+// 2. PROPOSE_BLOCK defines how often the contract can be upgraded. Defined in _setNextLogic() function and the interval here is set
 // to 172800 blocks ~1 month.
 // 3. Admin rights are burnable. Rather not do that without deadline
 // 4. prolongLock() allows to add to UPGRADE_BLOCK. Basically allows to prolong lock. Could prolong to maximum solidity number so the deadline might not be needed 
 // 5. logic contract is not being set suddenly. it's being stored in NEXT_LOGIC_SLOT for a month and only after that it can be set as LOGIC_SLOT.
 // Users have time to decide on if the deployer or the governance is malicious and exit safely.
 
-// It fixes upgradeability bug I believe. Consensys won't be so smug about it anymore.
+// It fixes "upgradeability bug" I believe.
 
-contract NamelessProxy {
+contract TrustMinimizedProxy {
 	event Upgraded(address indexed toLogic);
 	event AdminChanged(address indexed previousAdmin, address indexed newAdmin);
 	event NextLogicDefined(address indexed nextLogic);
-	event UpgradePostponed(uint toBlock);
+	event ProposalsRestrictedUntil(uint block);
 	event Canceled(address indexed toLogic);
 
 	bytes32 internal constant ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
 	bytes32 internal constant LOGIC_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-	bytes32 internal constant NEXT_LOGIC_SLOT = 0x56c185b2cb0723d5ac9bee49054a51e03ffce668e6ca209d91e6a1878e3ca4aa;
-	bytes32 internal constant NEXT_LOGIC_BLOCK_SLOT = 0x717ada2fcd4aad6ac93cdada14e28f6d4a8483da76e136464708d860266d8f95;
-	bytes32 internal constant UPGRADE_BLOCK_SLOT = 0x85e20208757fc820a8c68416a36f94ca955b4e86fafc028f8f82fb4c1f53c4ec;
-	bytes32 internal constant DEADLINE_SLOT = 0x82249145f5968d8bd05e6767411f4176a5fa34858f6fb467470c471c9a8f1d57;
+	bytes32 internal constant NEXT_LOGIC_SLOT = 0xb182d207b11df9fb38eec1e3fe4966cf344774ba58fb0e9d88ea35ad46f3601e;
+	bytes32 internal constant NEXT_LOGIC_BLOCK_SLOT = 0x96de003e85302815fe026bddb9630a50a1d4dc51c5c355def172204c3fd1c733;
+	bytes32 internal constant PROPOSE_BLOCK_SLOT = 0xbc9d35b69e82e85049be70f91154051f5e20e574471195334bde02d1a9974c90;
+	bytes32 internal constant DEADLINE_SLOT = 0xb124b82d2ac46ebdb08de751ebc55102cc7325d133e09c1f1c25014e20b979ad;
 
-	constructor(address logic) {
+	constructor(address logic, bytes memory data) payable {
 		require(ADMIN_SLOT == bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1) && LOGIC_SLOT == bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1) && // this require is simply against human error, can be removed if you know what you are doing
-		NEXT_LOGIC_SLOT == bytes32(uint256(keccak256('nameless.proxy.nextLogic')) - 1) && NEXT_LOGIC_BLOCK_SLOT == bytes32(uint256(keccak256('nameless.proxy.nextLogicBlock')) - 1) &&
-		UPGRADE_BLOCK_SLOT == bytes32(uint256(keccak256('nameless.proxy.upgradeBlock')) - 1) && DEADLINE_SLOT == bytes32(uint256(keccak256('nameless.proxy.deadline')) - 1), "code broke");
+		NEXT_LOGIC_SLOT == bytes32(uint256(keccak256('eip1984.proxy.nextLogic')) - 1) && NEXT_LOGIC_BLOCK_SLOT == bytes32(uint256(keccak256('eip1984.proxy.nextLogicBlock')) - 1) &&
+		PROPOSE_BLOCK_SLOT == bytes32(uint256(keccak256('eip1984.proxy.proposeBlock')) - 1) && DEADLINE_SLOT == bytes32(uint256(keccak256('eip1984.proxy.deadline')) - 1), "code broke");
 		_setAdmin(msg.sender);
+		if(data.length > 0) {
+			(bool success,) = logic.delegatecall(data);
+			require(success==true);
+		}
 		uint deadline = block.number + 4204800; // ~2 years as default
 		assembly {sstore(DEADLINE_SLOT,deadline) sstore(LOGIC_SLOT,logic)}
 	}
 	modifier ifAdmin() {if (msg.sender == _admin()) {_;} else {_fallback();}}
 
-	function getSettings() external ifAdmin returns(address logic, uint pgrdBlck, uint ddln) {return (_logic(), _upgradeBlock(), _deadline());}
+	function getSettings() external ifAdmin returns(address logic, uint pgrdBlck, uint ddln) {return (_logic(), _proposeBlock(), _deadline());}
 	function _logic() internal view returns (address logic) {assembly { logic := sload(LOGIC_SLOT) }}
-	function _upgradeBlock() internal view returns (uint bl) {assembly { bl := sload(UPGRADE_BLOCK_SLOT) }}
+	function _proposeBlock() internal view returns (uint bl) {assembly { bl := sload(PROPOSE_BLOCK_SLOT) }}
 	function _nextLogicBlock() internal view returns (uint bl) {assembly { bl := sload(NEXT_LOGIC_BLOCK_SLOT) }}
 	function _deadline() internal view returns (uint bl) {assembly { bl := sload(DEADLINE_SLOT) }}
 	function changeAdmin(address newAdm) external ifAdmin {emit AdminChanged(_admin(), newAdm);_setAdmin(newAdm);}
 	function proposeTo(address newLogic) external ifAdmin {_setNextLogic(newLogic);}
 	function proposeToAndCall(address newLogic, bytes calldata data) payable external ifAdmin {_setNextLogic(newLogic);(bool success,) = newLogic.delegatecall(data);require(success);}
-	function prolongLock(uint block_) external ifAdmin {uint ub; assembly {ub := sload(UPGRADE_BLOCK_SLOT) ub := add(ub,block_) sstore(UPGRADE_BLOCK_SLOT,ub)}emit UpgradePostponed(ub);}
+	function prolongLock(uint block_) external ifAdmin {uint pb; assembly {pb := sload(PROPOSE_BLOCK_SLOT) pb := add(pb,block_) sstore(PROPOSE_BLOCK_SLOT,pb)}emit ProposalsRestrictedUntil(pb);}
 
 	function _setNextLogic(address nextLogic) internal {
-		require(block.number >= _upgradeBlock() && block.number < _deadline(), "wait or too late");
+		require(block.number >= _proposeBlock() && block.number < _deadline(), "wait or too late");
 		require(_isContract(nextLogic), "Can't set to 0 bytecode");
-		uint upgradeBlock = block.number + 172800;
+		uint proposeBlock = block.number + 172800;
 		uint nextLogicBlock = block.number + 172800;
-		assembly { sstore(NEXT_LOGIC_SLOT, nextLogic) sstore(NEXT_LOGIC_BLOCK_SLOT, nextLogicBlock) sstore(UPGRADE_BLOCK_SLOT, upgradeBlock) }
+		assembly { sstore(NEXT_LOGIC_SLOT, nextLogic) sstore(NEXT_LOGIC_BLOCK_SLOT, nextLogicBlock) sstore(PROPOSE_BLOCK_SLOT, proposeBlock) }
 		emit NextLogicDefined(nextLogic);
 	}
 
