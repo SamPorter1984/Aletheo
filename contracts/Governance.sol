74a7a3381bcc48697722edccd64d7e2d8aab87b8
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-03-20 00:31:21 +0300

    Create Governance.sol

---
+++ contracts/Governance.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+interface ITreasury {function getTknAmntLckPt(address account)external view returns(uint amount,uint lock);}
+interface IFoundingEvent {function getFounderTknAmntLckPt(address account)external view returns(uint amount,uint lock);}
+
+pragma solidity >=0.7.0;
+
+import "./ITreasury.sol";
+import "./IFoundingEvent.sol";
+import "./IERC20.sol";
+
+contract Governance {
+	event ProposalCreated(uint id, address proposer, address destination, bytes data, uint endBlock);
+    event ExecuteProposal(uint id,address dstntn,bytes dt);
+    
+	struct Proposal {
+		address destination;
+		uint endBlock;
+		bool executed;
+		bool canceled;
+		uint forVotes;
+		uint againstVotes;
+		bytes data;
+		mapping (address => bool) votes;
+	}
+
+	uint private _proposalCount;
+	uint private _proposalThreshold;
+	address private _initializer;
+	address private _token;
+	address private _founding;
+	address private _treasury;
+	mapping(address => uint) private _votingPower;
+	mapping(address => uint) private _totalVotingPower;
+	mapping(address => uint) private _lock;
+	mapping(address => uint) private _lastPrivelegeCheck;
+	mapping(address => uint) private _lastVoted;
+	mapping(uint => Proposal) public proposals;
+	bool private _l;
+
+	constructor(){_initializer = msg.sender;} // anybody can deploy logic to propose it as an upgrade
+	function init() public {require(msg.sender == _initializer); delete _initializer;}
+
+	function propose(address dstntn,bytes memory dt) public {
+		uint vote;
+		if(block.number < _lastPrivelegeCheck[msg.sender] + 172800) {vote = _totalVotingPower[msg.sender];} else {vote = _votingPower[msg.sender];}
+		require(vote > 1000e18 && dstntn != address(0) && dt.length > 0 && _lock[msg.sender] - 1036800 >= block.number);
+		_proposalCount++;
+		uint id = _proposalCount;
+		uint endBlock = block.number + 172800;
+		proposals[id].destination = dstntn;
+		proposals[id].endBlock = endBlock;
+		proposals[id].data = dt;
+		proposals[id].forVotes = vote;
+		proposals[id].votes[msg.sender] = true;		
+		emit ProposalCreated(id, msg.sender, dstntn, dt, endBlock);
+	}
+
+	function castVote(uint id, bool support) external {
+		uint vote;
+		if(block.number < _lastPrivelegeCheck[msg.sender] + 172800) {vote = _totalVotingPower[msg.sender];} else {vote = _votingPower[msg.sender];} 
+		require(block.number<proposals[id].endBlock&&vote>0&&proposals[id].votes[msg.sender]==false&&_lock[msg.sender]-1036800>=block.number&&_lastVoted[msg.sender]+10<block.number);
+		if(support==true) {proposals[id].forVotes += vote;} else {proposals[id].againstVotes += vote;}
+		proposals[id].votes[msg.sender] = true;
+		_lastVoted[msg.sender] = block.number;
+	}
+
+	function lockFor3years(uint amount) external {
+		IERC20(_token).transferFrom(msg.sender,address(this),amount);
+		_votingPower[msg.sender] += amount;
+		_lock[msg.sender] += 6307200;
+	}
+
+	function unstake(uint amount) external {
+		require(_votingPower[msg.sender] >= amount && block.number>=_lock[msg.sender] && _l == false);
+		_l = true;
+		_votingPower[msg.sender] -= amount;
+		IERC20(_token).transfer(msg.sender,amount);
+		_l = false;
+	}
+
+	function resolveVoting(uint id) external {
+		uint forVotes = proposals[id].forVotes;
+		require(forVotes>=300e24 && block.number>=proposals[id].endBlock);//300 mil, depends on founders if there will be any executed proposals in first year
+		uint totalVotes = forVotes + proposals[id].againstVotes;
+		uint percent = 100*forVotes/totalVotes;
+		if(percent > 60) {_execute(id);}
+	}
+
+	function checkPrivelege() external {
+		uint totalVotingPower = 0;
+		(uint amount,uint lock) = IFoundingEvent(_founding).getFounderTknAmntLckPt(msg.sender);
+		if (lock == 0 || lock - 1036800 < block.number) {amount = 0;} 
+		totalVotingPower= _votingPower[msg.sender] + amount;
+		(amount,lock) = ITreasury(_treasury).getTknAmntLckPt(msg.sender);
+		if (lock == 0 || lock - 1036800 < block.number) {amount = 0;} 
+		totalVotingPower += amount;
+		_totalVotingPower[msg.sender] = totalVotingPower;
+		_lastPrivelegeCheck[msg.sender] = block.number;
+	}
+
+	function getLastVoted(address account) external view returns(uint lstVtd) {return _lastVoted[account];}
+
+	function changeAddress(address oldAccount,address newAccount) external {
+		_lastVoted[newAccount] = block.number;
+		_votingPower[newAccount] = _votingPower[oldAccount];
+		_lock[newAccount] = _lock[oldAccount];
+		delete _votingPower[oldAccount];
+		delete _lock[oldAccount];
+		delete _lastVoted[oldAccount];
+	}
+
+	function _execute(uint id) internal{
+		address dstntn = proposals[id].destination;
+		bytes memory dt = proposals[id].data;
+		dstntn.call(dt);
+		emit ExecuteProposal(id,dstntn,dt);
+	}
+}
