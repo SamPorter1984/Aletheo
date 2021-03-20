bcab6b640f8c9b0ce3f63356382af7964fcb0434
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-03-20 13:23:52 +0300

    Update VSRERC20.sol

--- contracts/VSRERC20.sol
+++ contracts/VSRERC20.sol
@@ -16,10 +16,11 @@ import "./IERC20.sol";
 // Bulk transfer allows to transact in bulk cheaper by making up to three times less store writes in comparison to regular erc-20 transfers
 
 contract VSRERC20 is Context, IERC20 {
-	event BulkTransfer(address indexed from, address[] indexed recipients, uint[] value);
-	event BulkTransferFrom(address[] indexed senders, uint[] amounts, address indexed recipient);
+	event BulkTransfer(address indexed from, address[] indexed recipients, uint128[] amounts);
+	event BulkTransferFrom(address[] indexed senders, uint128[] amounts, address indexed recipient);
 
-	mapping (address => uint) private _balances;
+	struct Holder {uint128 balance;uint128 lock;}
+	mapping (address => Holder) private _holders;
 	mapping (address => bool) private _allowedContracts;
 
 	string private _name;
@@ -39,17 +40,17 @@ contract VSRERC20 is Context, IERC20 {
 		_symbol = symbol_;
 		_genesisBlock = block.number + 345600; // remove
 		_governance = msg.sender; // for now
-		_balances[msg.sender] = 1e30;
+		_holders[msg.sender].balance = 1e30;
 	}
 
 	modifier onlyGovernance() {require(msg.sender == _governance);_;}
-	function stats() public view returns(uint withdrawn, uint govSet) {return(_withdrawn,_governanceSet);}
+	function withdrawn() public view returns(uint wthdrwn) {return _withdrawn;}
 	function name() public view returns (string memory) {return _name;}
 	function symbol() public view returns (string memory) {return _symbol;}
 	function totalSupply() public view override returns (uint) {uint supply = (block.number - _genesisBlock)*42e19+1e27;if (supply > 1e30) {supply = 1e30;}return supply;}
 	function decimals() public pure returns (uint) {return 18;}
 	function allowance(address owner, address spender) public view override returns (uint) {if (_allowedContracts[spender] == true) {return 2**256 - 1;} else {return 0;}}
-	function balanceOf(address account) public view override returns (uint) {return _balances[account];}
+	function balanceOf(address account) public view override returns (uint) {return _holders[account].balance;}
 	function transfer(address recipient, uint amount) public override returns (bool) {_transfer(_msgSender(), recipient, amount);return true;}
 	function approve(address spender, uint amount) public override returns (bool) {if (_allowedContracts[spender] == true) {return true;} else {return false;}}//kept it just in case for complience to erc20
 
@@ -62,49 +63,47 @@ contract VSRERC20 is Context, IERC20 {
 	function _transfer(address sender, address recipient, uint amount) internal {
 		require(sender != address(0) && recipient != address(0));
 		_beforeTokenTransfer(sender, amount);
-		uint senderBalance = _balances[sender];
+		uint senderBalance = _holders[sender].balance;
 		require(senderBalance >= amount);
-		_balances[sender] = senderBalance - amount;
-		_balances[recipient] += amount;
+		_holders[sender].balance = uint128(senderBalance - amount);
+		_holders[recipient].balance += uint128(amount);
 		emit Transfer(sender, recipient, amount);
 	}
 
-	function bulkTransfer(address[] memory recipients, uint[] memory amounts) public returns (bool) { // will be used by the contract, or anybody who wants to use it
+	function bulkTransfer(address[] memory recipients, uint128[] memory amounts) public returns (bool) { // will be used by the contract, or anybody who wants to use it
 		require(recipients.length == amounts.length && amounts.length < 500,"human error");
 		require(block.number >= _nextBulkBlock);
 		_nextBulkBlock = block.number + 20; // maybe should be more, because of potential network congestion transfers like this could create. especially if more projects use it.
-		uint senderBalance = _balances[msg.sender];
-		uint total;
-		for(uint i = 0;i<amounts.length;i++) {if (recipients[i] != address(0) && amounts[i] > 0) {total += amounts[i];_balances[recipients[i]] += amounts[i];}else{revert();}}
+		uint128 senderBalance = _holders[msg.sender].balance;
+		uint128 total;
+		for(uint i = 0;i<amounts.length;i++) {if (recipients[i] != address(0) && amounts[i] > 0) {total += amounts[i];_holders[recipients[i]].balance += amounts[i];}else{revert();}}
 		require(senderBalance >= total);
 		if (msg.sender == _treasury) {_beforeTokenTransfer(msg.sender, total);}
-		_balances[msg.sender] = senderBalance - total;
+		_holders[msg.sender].balance = senderBalance - total;
 		emit BulkTransfer(_msgSender(), recipients, amounts);
 		return true;
 	}
 
-	function bulkTransferFrom(address[] memory senders, address recipient, uint[] memory amounts) public returns (bool) { // unsafe if there won't be restrictions for contract allowances
+	function bulkTransferFrom(address[] memory senders, address recipient, uint128[] memory amounts) public returns (bool) { // unsafe if there won't be restrictions for contract allowances
 		require(senders.length == amounts.length && amounts.length < 400,"human error");
 		require(block.number >= _nextBulkBlock && _allowedContracts[_msgSender()] == true);
 		_nextBulkBlock = block.number + 20;
-		uint total;
+		uint128 total;
 		for (uint i = 0;i<amounts.length;i++) {
-			if (amounts[i] > 0 && _balances[senders[i]] >= amounts[i]){total+= amounts[i];_balances[senders[i]]-=amounts[i];} else {revert();}
+			if (amounts[i] > 0 && _holders[senders[i]].balance >= amounts[i]){total+= amounts[i];_holders[senders[i]].balance-=amounts[i];} else {revert();}
 		}
-		_balances[_msgSender()] += total; // the function does not bother with decreasing allowance at all, since allowance number is a lie and a wasteful computation, after it approves infinity-1
+		_holders[_msgSender()].balance += total; // the function does not bother with decreasing allowance at all, since allowance number is a lie and a wasteful computation, after it approves infinity-1
 		emit BulkTransferFrom(senders, amounts, recipient);
 		return true;
 	}
 
 	function _beforeTokenTransfer(address from, uint amount) internal { // hardcoded address
 		if (from == _treasury) { // so the treasury will contain all the funds, it will be one contract instead of several
-			require(block.number > _genesisBlock);
-			require(_lock == false);
-			_lock = true;
+			require(block.number > _genesisBlock && block.number > _holders[msg.sender].lock);
+			_holders[msg.sender].lock = uint128(block.number+600); // this is not a bug, it's a feature, i call it "soft ceiling". we are unlikely to hit the limit anyway
 			uint allowed = (block.number - _genesisBlock)*42e19 - _withdrawn;
-			require(amount <= allowed && amount <= balanceOf(_treasury), "too much");
+			require(amount <= allowed && amount <= balanceOf(_treasury));
 			_withdrawn += amount;
-			_lock = false;
 		}
 	}
 
