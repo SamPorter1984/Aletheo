9513285fdce1bbf4282babc647876de62a0fce0d
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-04-02 19:23:09 +0300

    Update FoundingEvent.sol

--- contracts/FoundingEvent.sol
+++ contracts/FoundingEvent.sol
@@ -8,9 +8,6 @@ pragma solidity >=0.7.0 <0.8.0;
 // The Event lasts for ~2 months to ensure fair distribution.
 // 0,5% of contributed Eth goes to developer for earliest development expenses including audits and bug bounties.
 // Blockchain needs no VCs, no authorities.
-// Tokens will be staked by default after liquidity will be created, so there is no stake function, and unstaking means losing Founder rewards forever.
-// I have moved rewards claiming and changing addresses logic to staking contract, which makes up for more efficient architecture. This contract is lighter
-// and so much easier to audit for investors.
 
 import "./IUniswapV2Factory.sol";
 import "./IUniswapV2Pair.sol";
@@ -21,22 +18,21 @@ import "./IStaking.sol";
 contract FoundingEvent {
 	mapping(address => uint) public contributions;
 	address payable private _deployer;
+	uint88 private _phase; // so what about potential 51% attack? so just in case we have to transfer in parts
 	bool private _lgeOngoing;
-	uint88 _phase; // so what about potential 51% attack? so just in case we have to transfer in parts
-	address _token;
-	uint96 _ETHDeposited;
 	address private _staking;
+	uint88 private _ETHDeposited;
 	bool private _notInit;
 
 	constructor() {_deployer = msg.sender;_notInit = true;}
-	function init(address c, address t) public {require(msg.sender == _deployer && _notInit == true);delete _notInit; _lgeOngoing = true; _staking = c; _token = t;}
+	function init(address c) public {require(msg.sender == _deployer && _notInit == true);delete _notInit; _lgeOngoing = true; _staking = c;}
 
 	function depositEth() external payable {
 		require(_lgeOngoing == true);
 		uint amount = msg.value;
 		if (block.number >= 12550000) {
 			uint phase = _phase;
-			if (phase > 0) {_ETHDeposited += amount;}
+			if (phase > 0) {_ETHDeposited += uint88(amount);}
 			if(block.number >= phase+12550000){_phase = uint88(phase + 10000);_createLiquidity(phase);}
 		}
 		else {uint deployerShare = amount/200; amount -= deployerShare; _deployer.transfer(deployerShare);}
@@ -48,13 +44,12 @@ contract FoundingEvent {
 		address token = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;// hardcoded token address after erc20 will be deployed
 		address staking = _staking; // has to be deployed before lge start
 		address tknETHLP = getPair[token][WETH];
-		if (phase == 0) {_ETHDeposited = address(this).balance; if (tknETHLP == address(0)) {IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).createPair(token, WETH);}}
+		if (phase == 0) {_ETHDeposited = uint88(address(this).balance); if (tknETHLP == address(0)) {IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).createPair(token, WETH);}}
 		uint ethToDeposit = _ETHDeposited*4/5;
 		uint tokenToDeposit = 1e23;
 		if (phase == 90000) {
-			ethToDeposit = address(this).balance;
-			IStaking(staking).init(_ETHDeposited, tknETHLP);
-			delete _staking; delete _lgeOngoing; delete _ETHDeposited; delete _phase;
+			ethToDeposit = address(this).balance; IStaking(staking).init(_ETHDeposited, tknETHLP);
+			delete _staking; delete _lgeOngoing; delete _ETHDeposited; delete _phase; delete _deployer;
 		}
 		IWETH(WETH).deposit{value: ethToDeposit}();
 		IERC20(token).transfer(tknETHLP, tokenToDeposit);
