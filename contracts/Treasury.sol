7ea9b3a0b5c8f1b5a0378442e43f73ee9babfb0b
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-04-03 20:50:10 +0300

    Update Treasury.sol

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -10,11 +10,12 @@ contract Treasury {
 	address private _oracleMain;
 	uint private _emission; // has to be very slow, like really slow, so that elected devs and officials will have incentive to support the project long-term
 	uint8 private _governanceSet;
+	bool private _init;
 
 	struct Beneficiary {uint88 amount; uint88 lastClaim; uint88 startBlock; bytes nameType;} // name is for transparency, type is like dev or mod, or bug bounty
 	mapping (address => Beneficiary) public bens;
 
-	constructor() {_governance = msg.sender; _emission = 1e15;}
+	function init() public {require(_init == false); _init=true; _governance = msg.sender; _emission = 1e15;}
 
 	function addBeneficiary(address a, uint amount, uint lastClaim, uint startBlock, bytes memory nameType) external {
 		require(amount<=30e21 && msg.sender == _governance);
@@ -24,7 +25,7 @@ contract Treasury {
 		bens[a].nameType = nameType;
 	}
 
-	function getBeneficiaryRewards() external onlyBeneficiary{
+	function getBeneficiaryRewards() external {
 		uint lastClaim = bens[msg.sender].lastClaim;
 		uint amount = bens[msg.sender].amount;
 		uint toClaim = (block.number - lastClaim)*_emission;
@@ -32,7 +33,6 @@ contract Treasury {
 		bens[msg.sender].lastClaim = uint88(block.number);
 		bens[acc].amount = uint88(amount - toClaim);
 		IERC20(_token).transfer(msg.sender, toClaim);
-		return true;
 	}
 
 	function getRewards(address acc,uint amount) external returns(bool res){ // for posters, providers and oracles
@@ -41,5 +41,6 @@ contract Treasury {
 	}
 	// if a majority votes for a beneficiary, then it probably won't be easy to get majority vote to remove same beneficiary. unless there is something really strange going on
 	function removeBeneficiary(address a) public {require(msg.sender == _governance); delete bens[a];}
-	function setGovernance(address a) public onlyGovernance {require(_governanceSet < 3);_governanceSet += 1;_governance = a;}
+	function setGovernance(address a) public {require(_governanceSet < 3);_governanceSet += 1;_governance = a;}
+	function setContracts(address j, address st, address om) public {require(msg.sender == _governance); _jobMarket = j; _staking = st; _oracleMain = om;}
 }
