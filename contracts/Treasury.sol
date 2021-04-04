5663059a3f8fb6edba1e9d3a467502a2474440d4
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-04-04 20:08:34 +0300

    Update Treasury.sol

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -7,21 +7,20 @@ contract Treasury {
 	address private _governance;
 	address private _jobMarket;
 	address private _staking;
-	address private _oracleMain;
+	address private _oracleMain;// oracle registry contract
 	uint56 private _emission; // has to be very slow, like really slow, so that elected devs and officials will have incentive to support the project long-term
 	uint8 private _governanceSet;
 	bool private _init;
 
-	struct Beneficiary {uint88 amount; uint88 lastClaim; uint88 startBlock; bytes nameType;} // name is for transparency, type is like dev or mod, or bug bounty
+	struct Beneficiary {uint88 amount; uint88 lastClaim; bytes nameType;} // name is for transparency, type is like dev or mod, or bug bounty
 	mapping (address => Beneficiary) public bens;
-
 	function init() public {require(_init == false); _init=true; _governance = msg.sender; _emission = 1e15;}
 
-	function addBeneficiary(address a, uint amount, uint lastClaim, uint startBlock, bytes memory nameType) external {
+	function addBeneficiary(address a, uint amount, uint lastClaim, bytes memory nameType) external {
 		require(amount<=30e21 && msg.sender == _governance);
 		bens[a].amount = uint88(amount);
-		bens[a].lastClaim = uint88(block.number);
-		bens[a].startBlock = uint88(startBlock);
+		if (block.number < 12640000) {lastClaim = 12640000;}
+		bens[a].lastClaim = uint88(lastClaim);
 		bens[a].nameType = nameType;
 	}
 
@@ -39,6 +38,7 @@ contract Treasury {
 		require(msg.sender == _jobMarket && msg.sender == _staking && msg.sender == _oracleMain);//hardcoded addresses
 		IERC20(_token).transfer(acc, amount); return true;
 	}
+
 	// if a majority votes for a beneficiary, then it probably won't be easy to get majority vote to remove same beneficiary. unless there is something really strange going on
 	function removeBeneficiary(address a) public {require(msg.sender == _governance); delete bens[a];}
 	function setGovernance(address a) public {require(_governanceSet < 3);_governanceSet += 1;_governance = a;}
