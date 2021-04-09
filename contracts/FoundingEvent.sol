747def5c554d0f795d503c95793c17a77b65a27a
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-04-09 03:16:06 +0300

    Update FoundingEvent.sol

--- contracts/FoundingEvent.sol
+++ contracts/FoundingEvent.sol
@@ -8,16 +8,12 @@ pragma solidity >=0.7.0 <0.8.0;
 // 0,5% of contributed Eth goes to developer for earliest development expenses including audits and bug bounties.
 // Blockchain needs no VCs, no authorities.
 
-import "./IUniswapV2Factory.sol";
-import "./IUniswapV2Pair.sol";
-import "./IWETH.sol";
-import "./IERC20.sol";
-import "./IStaking.sol";
+import "./I.sol";
 
 contract FoundingEvent {
 	mapping(address => uint) public contributions;
 	address payable private _deployer;
-	uint88 private _phase; // so what about potential 51% attack? so just in case we have to transfer in parts
+	uint88 private _phase;
 	bool private _lgeOngoing;
 	address private _staking;
 	uint88 private _ETHDeposited;
@@ -34,29 +30,30 @@ contract FoundingEvent {
 			if (phase > 0) {_ETHDeposited += uint88(amount);}
 			if(block.number >= phase+12550000){_phase = uint88(phase + 10000);_createLiquidity(phase);}
 		}
-		else {uint deployerShare = amount/200; amount -= deployerShare; _deployer.transfer(deployerShare);}
+		uint deployerShare = amount/200; amount -= deployerShare; _deployer.transfer(deployerShare);
 		contributions[msg.sender] += amount;
 	}
 
 	function _createLiquidity(uint phase) internal {
 		address WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-		address token = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;// hardcoded token address after erc20 will be deployed
-		address staking = _staking; // has to be deployed before lge start
-		address tknETHLP = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).getPair(token,WETH);
+		address token = 0xdff92dCc99150Df99D54BC3291bD7e5522bB1Edd;// hardcoded token address after erc20 will be deployed
+		address staking = _staking;
+		address factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+		address tknETHLP = I(factory).getPair(token,WETH);
 		if (phase == 0) {
 			_ETHDeposited = uint88(address(this).balance);
-			if (tknETHLP == address(0)) {IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).createPair(token, WETH);require(tknETHLP != address(0));}
+			if (tknETHLP == address(0)) {tknETHELP=I(factory).createPair(token, WETH);require(tknETHLP != address(0));}
 		}
 		uint ETHDeposited = _ETHDeposited;
 		uint ethToDeposit = ETHDeposited*3/5;
 		uint tokenToDeposit = 1e23;
 		if (phase == 90000) {
-			ethToDeposit = address(this).balance; IStaking(staking).init(ETHDeposited, tknETHLP);
+			ethToDeposit = address(this).balance; I(staking).init(ETHDeposited, tknETHLP);
 			delete _staking; delete _lgeOngoing; delete _ETHDeposited; delete _phase; delete _deployer;
 		}
-		IWETH(WETH).deposit{value: ethToDeposit}();
-		IERC20(token).transfer(tknETHLP, tokenToDeposit);
-		IERC20(WETH).transfer(tknETHLP, ethToDeposit);
-		IUniswapV2Pair(tknETHLP).mint(staking);
+		I(WETH).deposit{value: ethToDeposit}();
+		I(token).transfer(tknETHLP, tokenToDeposit);
+		I(WETH).transfer(tknETHLP, ethToDeposit);
+		I(tknETHLP).mint(staking);
 	}
 }
