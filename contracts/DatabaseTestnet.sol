ce82beda5cc22b0875498ba25a11e3bd0394f866
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-04-11 06:11:20 +0300

    Update DatabaseTestnet.sol

--- contracts/DatabaseTestnet.sol
+++ contracts/DatabaseTestnet.sol
@@ -14,7 +14,11 @@ contract DatabaseTestnet {
 
 	modifier onlyOracle() {require(_oracles[msg.sender] == true);_;}
 	modifier onlyGovernance() {require(msg.sender == _governance);_;}
-	function recordEntry(bytes32 _hash, string memory _entry) public {require(_blockNumbers[msg.sender] + 23 >= block.number);_blockNumbers[msg.sender] = block.number; emit Entry(msg.sender, _hash, _entry);}
+	function recordEntry(bytes32 _hash, string memory _entry) public {
+		uint bn = _blockNumbers[msg.sender];
+		require(bn + 23 >= block.number || bn == 0);
+		_blockNumbers[msg.sender] = block.number; emit Entry(msg.sender, _hash, _entry);
+	}
 
 	function toggleOracle(address[] memory accounts) public onlyGovernance {
 		for (uint i = 0; i < accounts.length; i++) {if (_oracles[accounts[i]] == false) {_oracles[accounts[i]] = true;} else {delete _oracles[accounts[i]];}}
