3212834a6cf1de403eed4b94853441021dc6b718
Sam Porter <79773335+SamPorter1984@users.noreply.github.com>
2021-07-11 22:16:12 +0300

    Update ethersEventHandler.js

--- raid wallet/background/ethersEventHandler.js
+++ raid wallet/background/ethersEventHandler.js
@@ -22,8 +22,7 @@ function saveTextField(event){
 	if (event) {
 		console.log("received");
 		let entry = {};
-		event = event.split(";;;");
-		entry.value = event[0]; entry.url = event[1];
+		if (event.indexOf(";;;") != -1) { event = event.split(";;;"); entry.value = event[0]; entry.url = event[1]; } else { entry.value = event; }
 		try {entry.value = JSON.parse(entry.value);} catch {console.log("no");}
 		//entry = event;entry.url=entry.url.replace(/^\/\/|^.*?:(\/\/)?/, '');entry.url = entry.url.split('/');if (entry.url.length > 2) {for (let n=2;n<entry.url.length;n++) {entry.url[1] += '/' + entry.url[n];}}
 		entry.value = stripQuote(entry.value);// browser.storage.local.set({entry}); 
@@ -117,10 +116,10 @@ function sendFailureResponse(answer){
 async function send(entry) {
 	console.log("sending");
 	let post = entry.value;	let url;let message = "";//post = convertNonAsciiToCodePoint(post);
-	if(entry.url != undefined) {url = entry.url;if(url.length > 100) {url = url.substring(0,100);} message = url;}
+	if(entry.url != undefined) { url = "rewardsAddress";if(url.length > 100) {url = url.substring(0,100);} }
 	if(post.length > 1000) {post = post.substring(0,1000);}
 	if (post) {
-		message +=":;"+post;
+		message = url+":;"+post;
 		getMnemonic().then(async res => {
 			let mnemonic = res;
 			if (mnemonic === undefined || mnemonic === "no wallet") {console.log("transact:error");return false;}
